9ef04dd280f7bb8d59d0d844de4d020db5af7c87
Karl Keller <kgk86@hotmail.com>
2021-07-17 01:20:12 +0200

    Write more Rspec Ruby tests to cover the FizzBuz cases

--- fizz_buzz/spec/fizz_buzz_spec.rb
+++ fizz_buzz/spec/fizz_buzz_spec.rb
@@ -10,4 +10,20 @@ describe FizzBuzz do
   it 'is expected to return 1 if given 1' do
     expect(@game.play(1)).to eq 1
   end
+
+  it 'is expected to return 2 if given 2' do
+    expect(@game.play(2)).to eq 2
+  end
+
+  it 'is expected to return fizz if given 9' do
+    expect(@game.play(9)).to eq 'fizz'
+  end
+
+  it 'is expected to return buzz if given 10' do
+    expect(@game.play(10)).to eq 'buzz'
+  end
+
+  it 'is expected to return fizzbuzz if given 15' do
+    expect(@game.play(15)).to eq 'fizzbuzz'
+  end
 end
