b1c833a18d453b52b400346e53bcfff86d162797
Karl Keller <kgk86@hotmail.com>
2021-07-17 01:16:19 +0200

    Write more Mocha JS tests to cover the FizzBuz cases

--- fizz_buzz/spec/fizzBuzz.spec.js
+++ fizz_buzz/spec/fizzBuzz.spec.js
@@ -11,4 +11,20 @@ describe('FizzBuzz ganme', () => {
   it('is expected to return 1 if given 1', () => {
     expect(game.play(1)).to.eql(1)
   })
+
+  it('is expected to return 2 if given 2', ()=> {
+    expect(game.play(2)).to.eql(2)
+  })
+
+  it('is expected to return fizz if given 9', ()=> {
+    expect(game.play(9)).to.eql('fizz')
+  })
+
+  it('is expected to return buzz if given 10', ()=> {
+    expect(game.play(10)).to.eql('buzz')
+  })
+
+  it('is expected to return fizzbuzz if given 15', ()=> {
+    expect(game.play(15)).to.eql('fizzbuzz')
+  })
 });
