ec72b2d2a477b9319c9a4b5bbc81e51943b5730e
Karl Keller <kgk86@hotmail.com>
2021-07-17 02:38:17 +0200

    JS: Return 'fizzbuzz'-numbers; all tests passing green

--- fizz_buzz/src/fizzBuzz.js
+++ fizz_buzz/src/fizzBuzz.js
@@ -1,8 +1,10 @@
 class FizzBuzz {
   play(number) {
-    if (number % 3 === 0) {
+    if (number % 15 === 0) {
+      return "fizzbuzz"
+    } else if (number % 3 === 0) {
       return "fizz"
-    } else if (number % 5 === 0){
+    } else if(number % 5 === 0) {
       return "buzz"
     } else {
       return number
