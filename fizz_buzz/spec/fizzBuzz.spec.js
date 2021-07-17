4235584202c0457f8f6176b8793f89e34260e954
Karl Keller <kgk86@hotmail.com>
2021-07-17 03:25:01 +0200

    Be consistent with spacing on arrow function syntax

--- fizz_buzz/spec/fizzBuzz.spec.js
+++ fizz_buzz/spec/fizzBuzz.spec.js
@@ -13,19 +13,19 @@ describe('FizzBuzz game', () => {
       expect(game.play(1)).to.eql(1)
     })
 
-    it('is expected to return 2 if given 2', ()=> {
+    it('is expected to return 2 if given 2', () => {
       expect(game.play(2)).to.eql(2)
     })
 
-    it('is expected to return fizz if given 9', ()=> {
+    it('is expected to return fizz if given 9', () => {
       expect(game.play(9)).to.eql('fizz')
     })
 
-    it('is expected to return buzz if given 10', ()=> {
+    it('is expected to return buzz if given 10', () => {
       expect(game.play(10)).to.eql('buzz')
     })
 
-    it('is expected to return fizzbuzz if given 15', ()=> {
+    it('is expected to return fizzbuzz if given 15', () => {
       expect(game.play(15)).to.eql('fizzbuzz')
     })
   })
