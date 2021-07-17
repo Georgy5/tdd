54378a477e26b5f6d8a30253a144ce10ec0622fd
Karl Keller <kgk86@hotmail.com>
2021-07-17 02:28:58 +0200

    Return 'fizzbuzz'-numbers

--- fizz_buzz/src/fizz_buzz.rb
+++ fizz_buzz/src/fizz_buzz.rb
@@ -1,6 +1,8 @@
 class FizzBuzz
   def play(number)
-    if number % 3 == 0
+    if number % 15 == 0
+      'fizzbuzz'
+    elsif number % 3 == 0
       'fizz'
     elsif number % 5 == 0
       'buzz'
