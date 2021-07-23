374940f8f2b71c0412b433e30b5b843df263af94
Karl Keller <kgk86@hotmail.com>
2021-07-23 12:14:22 +0200

    Fix test

--- e2e_demo/cypress/integration/userCanFillOutARegistrationForm.feature.js
+++ e2e_demo/cypress/integration/userCanFillOutARegistrationForm.feature.js
@@ -6,15 +6,15 @@ describe('user that fills out a registration form', () => {
       cy.visit('http://localhost:3474')
       
       cy.get('[data-cy=registration-form]').within(() => {
-        cy.get('[data-cy=email]').type('')
-        cy.get('[data-cy=password]').type()
+        cy.get('[data-cy=email]').type('test@email.com')
+        cy.get('[data-cy=password]').type('password')
         cy.get('[data-cy=submit]').click()
       })
     });
 
     it('is expected to see a confirmation message', () => {
       cy.get('[data-cy=confirmation-message]').should(
-        'containt.text',
+        'contain.text',
         'Thank you for registering!'
       );
     });
