9ff028bace1080d686f524dc170ff3d4970635bb
Karl Keller <kgk86@hotmail.com>
2023-05-21 19:23:51 +0200

    Update find Boomerang test cases

--- boomerang/spec/boomerang_spec.rb
+++ boomerang/spec/boomerang_spec.rb
@@ -1,27 +1,39 @@
 require_relative '../boomerang'
 
 describe Boomerang do
-  it 'is expected to return 1 if given 323' do
-    expect(subject.count([3, 2, 3])).to eq (1)
+  it 'is expected to return 1_found message if given 323' do
+    expect(subject.find([3, 2, 3])).to eq (
+      "1 Boomerang found: [[3, 2, 3]]"
+    )
   end
 
-  it 'is expected to return 0 if given 22' do
-    expect(subject.count([2, 2])).to eq (0)
+  it 'is expected to return none_found message if given 22' do
+    expect(subject.find([2, 2])).to eq (
+      "This list does not contain any Boomerangs"
+    )
   end
 
-  it 'is expected to return 2 if given 1212' do
-    expect(subject.count([1, 2, 1, 2])).to eq (2)
+  it 'is expected to return 2_found message if given 1212' do
+    expect(subject.find([1, 2, 1, 2])).to eq (
+      "2 Boomerangs found: [[1, 2, 1], [2, 1, 2]]"
+    )
   end
 
-  it 'is expected to return 3 if given 121234565' do
-    expect(subject.count([1, 2, 1, 2, 3, 4, 5, 6, 5])).to eq (3)
+  it 'is expected to return 3_found message if given 121234565' do
+    expect(subject.find([1, 2, 1, 2, 3, 4, 5, 6, 5])).to eq (
+      "3 Boomerangs found: [[1, 2, 1], [2, 1, 2], [5, 6, 5]]"
+    )
   end
 
-  it 'is expected to return 0 if given an empty array' do
-    expect(subject.count([])).to eq (0)
+  it 'is expected to return none_found message if given an empty array' do
+    expect(subject.find([])).to eq (
+      "This list does not contain any Boomerangs"
+    )
   end
 
-  it 'is expected to return 0 if given 55555' do
-    expect(subject.count([5, 5, 5, 5, 5])).to eq (0)
+  it 'is expected to return none_found message if given 55555' do
+    expect(subject.find([5, 5, 5, 5, 5])).to eq (
+      "This list does not contain any Boomerangs"
+    )
   end
 end
