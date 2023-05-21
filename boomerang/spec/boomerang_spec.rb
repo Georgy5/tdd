06ae67de1df40ff93df1c960fa3fdfcd3ebed2ed
Karl Keller <kgk86@hotmail.com>
2023-05-21 18:54:20 +0200

    Create first Boomerang test case

--- boomerang/spec/boomerang_spec.rb
+++ boomerang/spec/boomerang_spec.rb
@@ -1,7 +1,7 @@
 require_relative '../boomerang'
 
 describe Boomerang do
-  it 'is expected to return output RSpec' do
-    expect(Boomerang.run).to eq ("RSpec")
+  it 'is expected to return 1 if given 323' do
+    expect(subject.count([3, 2, 3])).to eq (1)
   end
 end
