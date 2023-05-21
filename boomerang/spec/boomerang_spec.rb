f87f3ac24898b1342c6c8906b3bdb3534690235e
Karl Keller <kgk86@hotmail.com>
2023-05-21 19:04:08 +0200

    Create third Boomerang test case

--- boomerang/spec/boomerang_spec.rb
+++ boomerang/spec/boomerang_spec.rb
@@ -8,4 +8,8 @@ describe Boomerang do
   it 'is expected to return 0 if given 22' do
     expect(subject.count([2, 2])).to eq (0)
   end
+
+  it 'is expected to return 2 if given 1212' do
+    expect(subject.count([1, 2, 1, 2])).to eq (2)
+  end
 end
