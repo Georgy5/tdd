22bd5b26cfa3792b30dfcdaf25260a482e254849
Karl Keller <kgk86@hotmail.com>
2023-05-21 19:26:44 +0200

    Update boomerang.rb for none found
    Change method to #find and
    pass tests for specs where no boomerangs are found.

--- boomerang/boomerang.rb
+++ boomerang/boomerang.rb
@@ -3,19 +3,26 @@
 # A boomerang is 3 lements where the first and last numbers are the same,
 # the middle number however is different.
 # eg  [1, 2, 1, 2] would be two boomerangs
+# output as: "2 Boomerangs found: [[1, 2, 1], [2, 1, 2]]"
 
 # thoughts:
 # iterate and compare groups of three numbers
 
 class Boomerang
-  def count(ary)
-    return 0 unless ary.size >= 3
-    count = 0
+  def find(ary)
+    none_found_message = "This list does not contain any Boomerangs"
+    if ary.length < 3
+      return none_found_message
+    end
+    counter = 0
     ary.each_with_index do |number, index|
       if ary[index] == ary[index + 2] and ary[index] != ary[index + 1]
-        count += 1
+        counter += 1
       end
     end
-    count
+
+    if counter == 0
+      return none_found_message
+    end
   end
 end
