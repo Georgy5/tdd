5027a8ddca4d02e33b39bd4af62bb7a31bbe093c
Karl Keller <kgk86@hotmail.com>
2023-06-21 19:25:27 +0200

    Add CodeWars PaginationHelper kata solution
    This is the solution that passes my tests.
    This solution failes one test on CodeWars:
    "page_item_count is returning incorrect value"
    `expect': page_item_count is returning incorrect value (Test::Error)

---
+++ pagination_helper/pagination_helper.rb
@@ -0,0 +1,62 @@
+# TODO: complete this class
+
+class PaginationHelper
+
+  # The constructor takes in an array of items and a integer indicating how many
+  # items fit within a single page
+  def initialize(collection, items_per_page)
+    @collection = collection
+    @items_per_page = items_per_page
+  end
+  
+  # returns the number of items within the entire collection
+  def item_count
+    @collection.size
+  end
+	
+  # returns the number of pages
+  def page_count
+    minimum = item_count / @items_per_page
+    page_count = minimum + 1 if remainder.nonzero?
+  end
+
+  def remainder
+    remainder = item_count % @items_per_page
+  end
+
+  def make_pages
+    pages = []
+    count = page_count
+    if remainder.nonzero?
+      pages.insert(0, @collection.pop(remainder))
+      count -= 1
+    end
+    count.times { pages.insert(0, @collection.pop(@items_per_page)) }
+    pages
+  end
+
+  def guard_clause(index)
+    if index >= page_count || index < 0
+      return -1
+    end
+  end
+	
+  # returns the number of items on the current page. page_index is zero based.
+  # this method should return -1 for page_index values that are out of range
+  def page_item_count(page_index)
+    guard_clause(page_index)
+    pages = make_pages
+    pages[page_index].nil? ? -1 : pages[page_index].size
+  end
+
+  # determines what page an item is on. Zero based indexes.
+  # this method should return -1 for item_index values that are out of range
+  def page_index(item_index)
+    guard_clause(item_index)
+    if item_index < item_count and item_index >= 0
+      item_index / @items_per_page
+    else
+      return -1
+    end
+  end
+end
