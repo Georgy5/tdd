66152e693232817a317b1fa4a7024f987edcf4ae
Karl Keller <kgk86@hotmail.com>
2023-10-02 10:55:54 +0200

    Fix spec

--- zeckendorf_number/spec/zeckendorf_spec.rb
+++ zeckendorf_number/spec/zeckendorf_spec.rb
@@ -2,6 +2,6 @@ require_relative '../zeckendorf'
 
 describe ZeckendorfTranslator do
   it 'is expected to return a Zeckendorf number of 0 if given the decimal 0' do
-    expect subject.translate(0).to eq 0
+    expect(subject.translate(0)).to eq(0)
   end
 end
