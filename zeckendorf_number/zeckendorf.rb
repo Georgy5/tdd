18a1d24c2cf08f0eb021a6a243a0eea38a42b0b5
Karl Keller <kgk86@hotmail.com>
2023-10-02 11:14:31 +0200

    zeckendorf.rb passing all the tests

--- zeckendorf_number/zeckendorf.rb
+++ zeckendorf_number/zeckendorf.rb
@@ -1,5 +1,19 @@
 class ZeckendorfTranslator
+  # List the first six distinct Fibonacci numbers
+  SIX_FIBS = [1, 2, 3, 5, 8, 13].freeze
+
   def translate(decimal)
-    decimal
+    if (decimal == 0)
+      return 0
+    end
+
+    zec = 0
+    SIX_FIBS.reverse.each_with_index do |fib, index|
+      if (decimal >= fib)
+        zec += 10**(5 - index)
+        decimal -= fib
+      end
+    end
+    return zec
   end
 end
