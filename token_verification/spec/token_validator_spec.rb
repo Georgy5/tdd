cbe8c7c049d6ff7fff41684dce1821c566d5f51c
Karl Keller <kgk86@hotmail.com>
2024-06-28 15:55:00 +0200

    Create initial TokenValidator Spec

---
+++ token_verification/spec/token_validator_spec.rb
@@ -0,0 +1,11 @@
+require_relative '../token_validator'
+
+describe TokenValidator do
+  it 'is expected that "-" removed, "/" replaces "!" and "+" replaces "$"' do
+    expect(subject.transform("Karl-G30r-GKel-ler!-$em!")).to eq("KarlG30rGKeller/+em/")
+  end
+
+  it 'is expected that the 15 bytes combined with XOR will yield 0' do
+    expect(subject.plausible?("Karl-G30r-GKel-ler5-KaRl")).to eq(0)
+  end
+end
