4d386caeb68c01383dc25b36974014f4953efdb3
Karl Keller <kgk86@hotmail.com>
2024-07-01 17:52:59 +0200

    Activate #plausible? Spec

--- token_verification/spec/token_validator_spec.rb
+++ token_verification/spec/token_validator_spec.rb
@@ -5,7 +5,7 @@ describe TokenValidator do
     expect(subject.transform("Karl-G30r-GKel-ler!-$em!")).to eq("KarlG30rGKeller/+em/")
   end
 
-  pending 'is expected that the 15 bytes combined with XOR will yield 0' do
+  it 'is expected that the 15 bytes combined with XOR will yield 0' do
     expect(subject.plausible?("Karl-G30r-GKel-ler5-KaRl")).to eq(0)
   end
 end
