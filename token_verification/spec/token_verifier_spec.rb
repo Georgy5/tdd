57d6aeb3bfc672e9e28bcfa57a275f4762540d83
Karl Keller <kgk86@hotmail.com>
2024-07-11 17:42:44 +0200

    Add more #plausible? tests to the Spec

--- token_verification/spec/token_verifier_spec.rb
+++ token_verification/spec/token_verifier_spec.rb
@@ -8,4 +8,16 @@ describe TokenVerifier do
   it 'is expected that the 15 bytes combined with XOR will yield 0' do
     expect(subject.plausible?("QUBs-WTOX-hEmh-6fY2-FGSl")).to eq(true)
   end
+
+  it 'is expected that the 15 bytes combined with XOR will yield 0' do
+    expect(subject.plausible?("YuL7-jIC6-lkKQ-MjEZ-4SNR")).to eq(true)
+  end
+
+  it 'is expected that the 15 bytes combined with XOR will yield 0' do
+    expect(subject.plausible?("yJqE-Yols-g0O0-kdq0-FLp0")).to eq(true)
+  end
+
+  it 'is expected that the 15 bytes combined with XOR will yield 0' do
+    expect(subject.plausible?("6!hV-4Psa-nECv-4f0R-nNF0")).to eq(true)
+  end
 end
