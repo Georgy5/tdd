dd62711acf35b6730e97d32dda516bd6b37e4578
Karl Keller <kgk86@hotmail.com>
2024-07-11 17:44:20 +0200

    Update #plausible? method logic

--- token_verification/token_verifier.rb
+++ token_verification/token_verifier.rb
@@ -1,10 +1,16 @@
+require 'base64'
+
 class TokenVerifier
   def transform(str)
     str.gsub('-', '').gsub('!', '/').gsub('$', '+')
   end
 
   def plausible?(str)
-    truncated_bytes = transform(str)[0, 15].bytes
-    truncated_bytes.reduce(0) { |acc, byte| acc ^ byte } == 0
+    truncated_str = transform(str)
+    decoded_bytes = Base64.decode64(truncated_str).bytes
+    return false unless decoded_bytes.length == 15
+
+    xor_result = decoded_bytes.reduce(0) { |acc, byte| acc ^ byte }
+    xor_result == 0
   end
 end
