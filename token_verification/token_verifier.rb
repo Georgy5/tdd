befb3d48cde5bd999576ff10d049bb70744be636
Karl Keller <kgk86@hotmail.com>
2024-07-11 16:21:47 +0200

    Update #plausible? to return a boolean

--- token_verification/token_verifier.rb
+++ token_verification/token_verifier.rb
@@ -5,6 +5,6 @@ class TokenVerifier
 
   def plausible?(str)
     truncated_bytes = transform(str)[0, 15].bytes
-    truncated_bytes.reduce(0) { |acc, byte| acc ^ byte }
+    truncated_bytes.reduce(0) { |acc, byte| acc ^ byte } == 0
   end
 end
